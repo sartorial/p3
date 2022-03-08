e70b86dc821161eb8ab873f144484f836256e276
sartorial <83042859+sartorial@users.noreply.github.com>
2022-03-07 20:58:36 -0500

    Add files via upload

---
+++ games.jsx
@@ -0,0 +1,163 @@
+const gamesarray ={
+    "games": [
+      {
+        "id": 1,
+        "title": "Gran Turismo 7",
+        "genre": "Sim Racing",
+        "release_date": "March, 04, 2022",
+        "summary": "Gran Turismo 7 is a 2022 sim racing video game developed by Polyphony Digital and published by Sony Interactive Entertainment. The game is the eighth mainline installment in the Gran Turismo series.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/1/14/Gran_Turismo_7_cover_art.jpg",
+        "platform": "PS5",
+        "price": 69.99
+      },
+      {
+        "id": 2,
+        "title": "Kirby and the Forgotten Land",
+        "genre": "Adventure Game",
+        "release_date": "March, 25, 2022",
+        "summary": "Kirby and the Forgotten Land is an upcoming 2022 platform video game developed by HAL Laboratory and published by Nintendo for the Nintendo Switch. It will be the seventeenth mainline installment in the Kirby series, as well as the first mainline game in the series in full 3D, excluding spin-offs.",
+        "image": "https://images.nintendolife.com/1ddb829d9a730/kirby-and-the-forgotten-land-cover.cover_large.jpg",
+        "platform": "Nintendo Switch",
+        "price": 59.99  
+      },
+      {
+        "id": 3,
+        "title": "Fire Emblem Warriors: Three Hopes",
+        "genre": "Adventure Game",
+        "release_date": "June, 24, 2022",
+        "summary": "Fire Emblem Warriors: Three Hopes is an upcoming hack and slash action role-playing game developed by Omega Force and Team Ninja, and published by Nintendo for the Nintendo Switch.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/1/14/Gran_Turismo_7_cover_art.jpg",
+        "platform": "Nintendo Switch",
+        "price": 59.99  
+      },
+      {
+        "id": 4,
+        "title": "Nintendo Switch Sports",
+        "genre": "Sim Game",
+        "release_date": "April, 29, 2022",
+        "summary": "Nintendo Switch Sports is an upcoming sports simulation video game developed and published by Nintendo for the Nintendo Switch. It is the third main entry in the Wii Sports series of games. The game's release is scheduled for April 29, 2022.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/thumb/f/f0/Nintendo_Switch_Sports_logo_icon.png/220px-Nintendo_Switch_Sports_logo_icon.png",
+        "platform": "Nintendo Switch",
+        "price": 39.99  
+      },
+      {
+        "id": 5,
+        "title": "Mario Strikers: Battle League",
+        "genre": "Sim Game",
+        "release_date": "June, 10, 2022",
+        "summary": "Mario Strikers: Battle League, known in Europe and Australia as Mario Strikers: Battle League Football, is an upcoming association football/soccer video game developed by Next Level Games and Nintendo EPD and published by Nintendo. It is scheduled for a worldwide release on the Nintendo Switch on June 10, 2022.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/thumb/4/4e/Mario_strikers_battle_league.jpeg/220px-Mario_strikers_battle_league.jpeg",
+        "platform": "Nintendo Switch",
+        "price": 59.99  
+      },
+      {
+        "id": 6,
+        "title": "Gran Turismo 7",
+        "genre": "Sim Racing",
+        "release_date": "March, 04, 2022",
+        "summary": "Gran Turismo 7 is a 2022 sim racing video game developed by Polyphony Digital and published by Sony Interactive Entertainment. The game is the eighth mainline installment in the Gran Turismo series.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/1/14/Gran_Turismo_7_cover_art.jpg",
+        "platform": "Nintendo Switch",
+        "price": 69.99  
+      },
+      {
+        "id": 7,
+        "title": "Gran Turismo 7",
+        "genre": "Sim Racing",
+        "release_date": "March, 04, 2022",
+        "summary": "Gran Turismo 7 is a 2022 sim racing video game developed by Polyphony Digital and published by Sony Interactive Entertainment. The game is the eighth mainline installment in the Gran Turismo series.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/1/14/Gran_Turismo_7_cover_art.jpg",
+        "platform": "Nintendo Switch",
+        "price": 69.99  
+      },
+      {
+        "id": 8,
+        "title": "Gran Turismo 7",
+        "genre": "Sim Racing",
+        "release_date": "March, 04, 2022",
+        "summary": "Gran Turismo 7 is a 2022 sim racing video game developed by Polyphony Digital and published by Sony Interactive Entertainment. The game is the eighth mainline installment in the Gran Turismo series.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/1/14/Gran_Turismo_7_cover_art.jpg",
+        "platform": "Nintendo Switch",
+        "price": 69.99  
+      },
+      {
+        "id": 9,
+        "title": "Gran Turismo 7",
+        "genre": "Sim Racing",
+        "release_date": "March, 04, 2022",
+        "summary": "Gran Turismo 7 is a 2022 sim racing video game developed by Polyphony Digital and published by Sony Interactive Entertainment. The game is the eighth mainline installment in the Gran Turismo series.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/1/14/Gran_Turismo_7_cover_art.jpg",
+        "platform": "Nintendo Switch",
+        "price": 69.99  
+      },
+      {
+        "id": 10,
+        "title": "Gran Turismo 7",
+        "genre": "Sim Racing",
+        "release_date": "March, 04, 2022",
+        "summary": "Gran Turismo 7 is a 2022 sim racing video game developed by Polyphony Digital and published by Sony Interactive Entertainment. The game is the eighth mainline installment in the Gran Turismo series.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/1/14/Gran_Turismo_7_cover_art.jpg",
+        "platform": "Nintendo Switch",
+        "price": 69.99  
+      },
+      {
+        "id": 11,
+        "title": "Gran Turismo 7",
+        "genre": "Sim Racing",
+        "release_date": "March, 04, 2022",
+        "summary": "Gran Turismo 7 is a 2022 sim racing video game developed by Polyphony Digital and published by Sony Interactive Entertainment. The game is the eighth mainline installment in the Gran Turismo series.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/1/14/Gran_Turismo_7_cover_art.jpg",
+        "platform": "Nintendo Switch",
+        "price": 69.99  
+      },
+      {
+        "id": 12,
+        "title": "Gran Turismo 7",
+        "genre": "Sim Racing",
+        "release_date": "March, 04, 2022",
+        "summary": "Gran Turismo 7 is a 2022 sim racing video game developed by Polyphony Digital and published by Sony Interactive Entertainment. The game is the eighth mainline installment in the Gran Turismo series.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/1/14/Gran_Turismo_7_cover_art.jpg",
+        "platform": "Nintendo Switch",
+        "price": 69.99  
+      },
+      {
+        "id": 13,
+        "title": "Gran Turismo 7",
+        "genre": "Sim Racing",
+        "release_date": "March, 04, 2022",
+        "summary": "Gran Turismo 7 is a 2022 sim racing video game developed by Polyphony Digital and published by Sony Interactive Entertainment. The game is the eighth mainline installment in the Gran Turismo series.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/1/14/Gran_Turismo_7_cover_art.jpg",
+        "platform": "Nintendo Switch",
+        "price": 69.99  
+      },
+      {
+        "id": 14,
+        "title": "Gran Turismo 7",
+        "genre": "Sim Racing",
+        "release_date": "March, 04, 2022",
+        "summary": "Gran Turismo 7 is a 2022 sim racing video game developed by Polyphony Digital and published by Sony Interactive Entertainment. The game is the eighth mainline installment in the Gran Turismo series.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/1/14/Gran_Turismo_7_cover_art.jpg",
+        "platform": "Nintendo Switch",
+        "price": 69.99  
+      },
+      {
+        "id": 15,
+        "title": "Gran Turismo 7",
+        "genre": "Sim Racing",
+        "release_date": "March, 04, 2022",
+        "summary": "Gran Turismo 7 is a 2022 sim racing video game developed by Polyphony Digital and published by Sony Interactive Entertainment. The game is the eighth mainline installment in the Gran Turismo series.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/1/14/Gran_Turismo_7_cover_art.jpg",
+        "platform": "Nintendo Switch",
+        "price": 69.99  
+      },
+      {
+        "id": 16,
+        "title": "Gran Turismo 7",
+        "genre": "Sim Racing",
+        "release_date": "March, 04, 2022",
+        "summary": "Gran Turismo 7 is a 2022 sim racing video game developed by Polyphony Digital and published by Sony Interactive Entertainment. The game is the eighth mainline installment in the Gran Turismo series.",
+        "image": "https://upload.wikimedia.org/wikipedia/en/1/14/Gran_Turismo_7_cover_art.jpg",
+        "platform": "Nintendo Switch",
+        "price": 69.99  
+      },
+    ]}
